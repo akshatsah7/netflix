5af4378fbafbfdaa97ad9e8efe45effd1ef8668b
akshatsah7 <akshatsah63@gmail.com>
2024-04-05 12:46:01 +0530

    Updated font and bg image

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -34,7 +34,7 @@ const Movie = ({ item }) => {
         src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
         alt={item?.title}
       />
-      <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+      <div class='absolute top-0 left-0 w-full h-full bg-black/80 opacity-0 text-white hover:opacity-100 hover:scale-105 transition duration-300 transform origin-top-left'>
         <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
           {item?.title}
         </p>
